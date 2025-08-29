4c4f322856ffff03d21f81f746c1e64e25d5f0d8
mostafizur89 <mr4020089@gmail.com>
2025-08-29 22:56:46 +0600

    added love count section

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+// coin section
+
+// let coins = 100;
+// const coinBtn = document.getElementById("coin-btn");
+// const historyCard = document.getElementById("history-card");
+
+// document.querySelectorAll(".call-btn").forEach(call-btn => {
+//     call-btn.addEventListener("click", () => {
+//         if (coins >= 20){
+//             coins -=20;
+//             coinBtn.textContent = coins: ${coins}
+//             alert(calling ${call-btn.textContent }...);
+//             if(history-card.textContent === "no call "){
+//                historyCard.textContent = "";
+//             }
+//             historyCard.innerHTML += <p>called: ${call-btn.textContent}</p>
+//         }
+//         else{
+//             alert("Not enough sufficient coin")
+//         }
+//     })
+// })
+
+
+
 // hearts Selection
 
 
@@ -11,3 +36,5 @@ const hearts = document.querySelectorAll(".heart-btn");
 for(const heart of hearts){
     heart.addEventListener("click", incressLoveCount)
 }
+
+
